1a072e00aec62c8df97d2c762e51c2dfc57a5e58
Darwin  Corral <darwin.corral@quito.gob.ec>
2020-08-13 17:56:05 -0500

    Servicios de Prueba

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,9 @@
 export const environment = {
   //APIS
   URL_SERVICIOS_SEGURIDAD : 'http://192.168.1.119:10021/micro_seguridades/usuario',
-  URL_SERVICIOS:'',
+  URL_SERVICIOS:'http://192.168.1.119:10021/testApiMatVehNuevos',
   URL_SERVICIOS_CORREO : 'http://172.20.68.122:8080/micro_sendMail-0.0.1/mail/sendBasic',
+  URL_REPORTE:'http://192.168.1.119:10021/reporte_nuevos',
 
   //rango de paginacion
   rango : 10,
