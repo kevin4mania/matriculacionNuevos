90fdad347c74accf51c80b87d6147f4f393561e3
Darwin  Corral <darwin.corral@quito.gob.ec>
2020-08-28 01:55:16 -0500

    Se cambia url's de pruebas por url's de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,8 @@
 export const environment = {
   //APIS
-  URL_SERVICIOS_SEGURIDAD : 'http://192.168.1.119:10021/micro_seguridades/usuario',
-  URL_SERVICIOS:'http://192.168.1.119:10021/testApiMatVehNuevos',
-  URL_SERVICIOS_CORREO : 'http://172.20.68.122:8080/micro_sendMail-0.0.1/mail/sendBasic',
-  URL_REPORTE:'http://192.168.1.119:10021/reporte_nuevos',
-  URL_SERVICIOS_FILE: 'http://192.168.1.53:8180/api_ftp-0.0.1/ftp',
-  URL_SERVIDOR_PHOTOS : 'http://192.168.1.181/',
+  URL_SERVICIOS: 'http://servicios.amt.gob.ec:10319/apiMatVehNuevos',
+  URL_SERVICIOS_CORREO : 'http://186.47.204.233:8080/micro_sendMail-0.0.1/mail/sendBasic',
+  URL_REPORTE:'http://servicios.amt.gob.ec:10319/reporte_nuevos',
 
   //rango de paginacion
   rango : 10,
@@ -13,6 +10,8 @@ export const environment = {
 
   //ID DE APLICACION DE LA BD
   idAplicacion: '4',
+  //URL DE APLICACION
+  url:'http://servicios.amt.gob.ec/web/matriculacionNuevos/',
   //NOMBRE DE APLICACION
   nombreAplicaion:'matriculacionNuevos',
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,17 @@
 
 export const environment = {
   //APIS
-  URL_SERVICIOS_SEGURIDAD : 'http://192.168.1.119:10021/micro_seguridades/usuario',
   URL_SERVICIOS:'http://192.168.1.119:10021/testApiMatVehNuevos',
   URL_SERVICIOS_CORREO : 'http://172.20.68.122:8080/micro_sendMail-0.0.1/mail/sendBasic',
   URL_REPORTE:'http://192.168.1.119:10021/reporte_nuevos',
-  URL_SERVICIOS_FILE: 'http://192.168.1.53:8180/api_ftp-0.0.1/ftp',
-  URL_SERVIDOR_PHOTOS : 'http://192.168.1.181/',
   //rango de paginacion
   rango : 10,
 
 
   //ID DE APLICACION DE LA BD
   idAplicacion: '43',
+  //URL DE APLICACION
+  url:'http://localhost:4200/',
   //NOMBRE DE APLICACION
   nombreAplicaion:'matriculacionNuevos',
 
